73141b3a88356a33b944d95242064e06169da9ed
Bernhard Six <Bernhard.Six@gmail.com>
2016-09-27 20:43:08 +0200

    tango app, second commit

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse(
+        "Hallo World<br /><a href='/rango/about'>About</a>")
+
+
+def about(request):
+    return HttpResponse(
+        "This is the about page <br /><a href ='/rango/'>Home</a>")
